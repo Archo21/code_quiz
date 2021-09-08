ee0af37c13d8b8df917942a62ac31c04985df050
Archo21 <87740574+Archo21@users.noreply.github.com>
2021-09-07 18:42:58 -0700

    final

--- script.js
+++ script.js
@@ -1,53 +1,64 @@
+//Variables
 var startButton=document.getElementById("start-btn")
 var timerEl=document.getElementById("timeCountdownText")
 var question1=document.getElementById("Question1")
 var question2=document.getElementById("Question2")
 var question3=document.getElementById("Question3")
 var nextButton=document.getElementById("next-btn")
-var timer=50
+var timer=30
 var timerid;
 var questionIndex=[question1,question2,question3]
 var correctAnswer=["USA","Japan","China"]
 var currentQuestionIndex=0
-function timerStart(){
+var correctCount=0
+var wrongCount=0
 
- timer--;
+//start timer
+function timerStart(){
+  timer--;
   timerEl.textContent=timer
-  if (timer <=0){
-     
-    console.log("quiz is over")
+  if (timer < 0 ){ 
+    console.log("quiz is over");
+    timerEl.textContent=0
+  
   }
 }
+//Next Question
 function nextQuestion(){
-if(questionIndex[currentQuestionIndex-1]){
-  var previuosQuestion=questionIndex[currentQuestionIndex-1]
-previuosQuestion.setAttribute("class","hide")
+  if(questionIndex[currentQuestionIndex-1]){
+    var previuosQuestion=questionIndex[currentQuestionIndex-1]
+    previuosQuestion.setAttribute("class","hide")
+  }
+
+  var currentQuestion=questionIndex[currentQuestionIndex]
+  currentQuestion.setAttribute("class","show")
+  console.log(currentQuestion);
 }
-var currentQuestion=questionIndex[currentQuestionIndex]
-currentQuestion.setAttribute("class","show")
-console.log(currentQuestion);
-}
+//Selected Ansswer
 function selectedAnswer(){
   if ($('input[name="Country"]:checked').val()==correctAnswer[currentQuestionIndex]){
     console.log("Correct");
-
+    correctCount++
+  } else{
+    console.log("Wrong");
+    wrongCount++
+    timer=timer-10
   }
-else{
-  console.log("Wrong");
-}
-currentQuestionIndex++;
-if(currentQuestionIndex===questionIndex.length){
-  
-  quizEnd();
-  
-}
-else{
 
-  nextQuestion()
+  currentQuestionIndex++;
+  if(currentQuestionIndex==questionIndex.length){
+    questionIndex[currentQuestionIndex-1].hidden=true
+    scores()
+    clearTimeout(timerid)
+  } else {
+    nextQuestion()
+  }
 }
+//Display score
+function scores(){
+  nextButton.textContent = "Quiz is Over Score: " + (correctCount/(correctCount + wrongCount)) * 100 + "%"
 }
 
-//quiz start
 function quizStart(){
   nextQuestion()
   startButton.setAttribute("class","hide")
