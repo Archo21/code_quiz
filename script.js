e628f26ff5ae16b1fba73f91c45c6a98dcd1d9de
Archo21 <87740574+Archo21@users.noreply.github.com>
2022-01-29 10:00:34 -0800

    new Aprroach

--- script.js
+++ script.js
@@ -1,68 +1,240 @@
-//Variables
-var startButton=document.getElementById("start-btn")
-var timerEl=document.getElementById("timeCountdownText")
-var question1=document.getElementById("Question1")
-var question2=document.getElementById("Question2")
-var question3=document.getElementById("Question3")
-var nextButton=document.getElementById("next-btn")
-var timer=30
-var timerid;
-var questionIndex=[question1,question2,question3]
-var correctAnswer=["USA","Japan","China"]
-var currentQuestionIndex=0
-var correctCount=0
-var wrongCount=0
 
-//start timer
-function timerStart(){
-  timer--;
-  timerEl.textContent=timer
-  if (timer < 0 ){ 
-    console.log("quiz is over");
-    timerEl.textContent=0
-  
-  }
-}
-//Next Question
-function nextQuestion(){
-  if(questionIndex[currentQuestionIndex-1]){
-    var previuosQuestion=questionIndex[currentQuestionIndex-1]
-    previuosQuestion.setAttribute("class","hide")
-  }
+let timeEl = document.querySelector("p.time");
+let secondsLeft = 75;
+let scoreEl = document.querySelector("#score");
 
-  var currentQuestion=questionIndex[currentQuestionIndex]
-  currentQuestion.setAttribute("class","show")
-  console.log(currentQuestion);
-}
-//Selected Ansswer
-function selectedAnswer(){
-  if ($('input[name="Country"]:checked').val()==correctAnswer[currentQuestionIndex]){
-    console.log("Correct");
-    correctCount++
-  } else{
-    console.log("Wrong");
-    wrongCount++
-    timer=timer-10
-  }
+// sections
+// section intro
+const introEl = document.querySelector("#intro");
 
-  currentQuestionIndex++;
-  if(currentQuestionIndex==questionIndex.length){
-    questionIndex[currentQuestionIndex-1].hidden=true
-    scores()
-    clearTimeout(timerid)
-  } else {
-    nextQuestion()
-  }
-}
-//Display score
-function scores(){
-  nextButton.textContent = "Quiz is Over Score: " + (correctCount/(correctCount + wrongCount)) * 100 + "%"
+// section questions
+//question section
+const questionsEl = document.querySelector("#questions");
+//where question goes
+let questionEl = document.querySelector("#question");
+// how many questions they have answered
+let questionCount = 0;
+// div yaynay
+const yaynayEl = document.querySelector("#yaynay");
+
+// section final
+const finalEl = document.querySelector("#final");
+// user initials
+let initialsInput = document.querySelector("#initials");
+
+// section highscores
+const highscoresEl = document.querySelector("#highscores");
+// ordered list
+let scoreListEl = document.querySelector("#score-list");
+// array of scores
+let scoreList = [];
+
+// buttons
+// start
+const startBtn = document.querySelector("#start");
+// answer button class
+const ansBtn = document.querySelectorAll("button.ansBtn")
+// answer1
+const ans1Btn = document.querySelector("#answer1");
+// answer2
+const ans2Btn = document.querySelector("#answer2");
+// answer3
+const ans3Btn = document.querySelector("#answer3");
+// answer4
+const ans4Btn = document.querySelector("#answer4");
+// submit-score
+const submitScrBtn = document.querySelector("#submit-score");
+// goback
+const goBackBtn = document.querySelector("#goback");
+// clearscores
+const clearScrBtn = document.querySelector("#clearscores");
+// view-scores
+const viewScrBtn = document.querySelector("#view-scores");
+
+// Object for question, answer, true/false
+const questions = [ // array of objects
+    {
+        
+        question: "Which country is leading in long distance races in the world",
+        answers: ["1.Kenya ", "2. Ethiopia", "3. Great Brintain", "4. USA"],
+        correctAnswer: "1"
+    },
+    {
+        question: "Which country lead in olympic  medal count in 2021?",
+        answers: ["1. China", "2. USA", "3. Japan", "4. Great Britain"],
+        correctAnswer: "2"
+    },
+    {
+    
+        question: "Which country hosted olympic in 2021?.",
+        answers: ["1. Jamaica", "2. Angola", "3. Japan", "4. China"],
+        correctAnswer: "3"
+    },
+    {
+        
+        question: "Which country was second in medal count in olympic 2021?.",
+        answers: ["1. USA", "2. Kenya", "3. Japan", "4. China"],
+        correctAnswer: "4"
+    },
+    {
+
+        // question: "Which continent lead in long Distance races in the world ?.",
+        // answers: ["1. Asia", "2. North America", "3. Africa", "4. South America"],
+        // correctAnswer: "4"
+    }
+];
+
+
+// Functions
+
+// timer
+function setTime() {
+    let timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = `Time:${secondsLeft}s`;
+
+        if (secondsLeft === 0 || questionCount === questions.length) {
+            clearInterval(timerInterval);
+            questionsEl.style.display = "none";
+            finalEl.style.display = "block";
+            scoreEl.textContent = secondsLeft;
+        }
+    }, 1000);
 }
 
-function quizStart(){
-  nextQuestion()
-  startButton.setAttribute("class","hide")
-  timerid=setInterval(timerStart,1000)
+// start quiz with timer and set up questions
+function startQuiz() {
+    //introEl.style.display = "inline block";
+    questionsEl.style.display = "block";
+    questionCount = 0;
+
+    setTime();
+    setQuestion(questionCount);
 }
-startButton.onclick=quizStart
-nextButton.onclick=selectedAnswer
+
+// function to set question; takes in a count and displays the next question/answers
+function setQuestion(id) {
+    if (id < questions.length) {
+        questionEl.textContent = questions[id].question;
+        ans1Btn.textContent = questions[id].answers[0];
+        ans2Btn.textContent = questions[id].answers[1];
+        ans3Btn.textContent = questions[id].answers[2];
+        ans4Btn.textContent = questions[id].answers[3];
+        
+    }
+}
+
+// function to check answer and then move to next question
+function checkAnswer(event) {
+    event.preventDefault();
+
+    // show section for yaynay and append message
+    yaynayEl.style.display = "block";
+    let p = document.createElement("p");
+    yaynayEl.appendChild(p);
+
+    // time out after 1 second
+    setTimeout(function () {
+        p.style.display = 'none';
+    }, 1000);
+
+    // answer checker
+    if (questions[questionCount].correctAnswer === event.target.value) {
+        p.textContent = "Correct!";
+    } else if (questions[questionCount].correctAnswer !== event.target.value) {
+        secondsLeft = secondsLeft - 10;
+        p.textContent = "Wrong!";
+    }
+
+    // increment so the questions index is increased
+    if (questionCount < questions.length) {
+        questionCount++;
+    }
+    // call setQuestion to bring in next question when any ansBtn is clicked
+    setQuestion(questionCount);
+}
+
+function addScore(event) {
+    event.preventDefault();
+
+    finalEl.style.display = "none";
+    highscoresEl.style.display = "block";
+
+    let init = initialsInput.value.toUpperCase();
+    scoreList.push({ initials: init, score: secondsLeft });
+
+    // sort scores
+    scoreList = scoreList.sort((a, b) => {
+        if (a.score < b.score) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    
+    scoreListEl.innerHTML="";
+    for (let i = 0; i < scoreList.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${scoreList[i].initials}: ${scoreList[i].score}`;
+        scoreListEl.append(li);
+    }
+
+    // Add to local storage
+    storeScores();
+    displayScores();
+}
+
+function storeScores() {
+    localStorage.setItem("scoreList", JSON.stringify(scoreList));
+}
+
+function displayScores() {
+    // Get stored scores from localStorage
+    // Parsing the JSON string to an object
+    let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
+
+    // If scores were retrieved from localStorage, update the scorelist array to it
+    if (storedScoreList !== null) {
+        scoreList = storedScoreList;
+    }
+}
+
+// clear scores
+function clearScores() {
+    localStorage.clear();
+    scoreListEl.innerHTML="";
+}
+
+// EventListeners
+// Start timer and display first question when click start quiz
+startBtn.addEventListener("click", startQuiz);
+
+// Check answers loop
+ansBtn.forEach(item => {
+    item.addEventListener('click', checkAnswer);
+});
+
+// Add score
+submitScrBtn.addEventListener("click", addScore);
+
+// Go Back Button
+goBackBtn.addEventListener("click", function () {
+    highscoresEl.style.display = "none";
+    introEl.style.display = "block";
+    secondsLeft = 75;
+    timeEl.textContent = `Time:${secondsLeft}s`;
+});
+
+// Clear the scores
+clearScrBtn.addEventListener("click", clearScores);
+
+// View/Hide High Scores Button
+viewScrBtn.addEventListener("click", function () {
+    if (highscoresEl.style.display === "none") {
+        highscoresEl.style.display = "block";
+    } else if (highscoresEl.style.display === "block") {
+        highscoresEl.style.display = "none";
+    } else {
+        return alert("No scores to show.");
+    }
+});
